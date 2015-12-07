635c8a4578c9c5b06557541d08af763a35120a32
Yutong Pan <yutonp@uw.edu>
2015-12-06 21:56:38 -0800

    comment

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 // Set up cannot be named differently, constructs canvas
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+	// Set up createCanvas() in function setup() with parameters w = displayWidth and h = displayHeight
 
 }
+
 
 // draw cannot be named differently, will be called in a loop
 function draw() {
-  stroke(150);
+	stroke(255);
+
+	// Draw ellipse() in function draw() with parameters x = 100 y = 100 w = 50 h = 50
+
+	// Make mouseX and mouseY the x and y parameters of ellipse
+
+	// fill() the ellipse to blue using R, G, B integer values
+
+	// Make mouseX and mouseY the R and G parameters of fill
+
+	// Add a mouse press event in your draw() function to call flower()
+
 }
 
+
+
 function flower() {
-  stroke(255);
-  fill(255, 0, 100, 255);
-  translate(mouseX, mouseY);
-  for (var i = 0; i < 6; i++) {
-    ellipse(0, 0, 20, 80);
-    rotate(PI/6);
-  }
+  	// Set the stroke() color to white and fill the flower to a color using any R, G, B values
+
+  	// translate() the flower with your mouse position
+
+  	// Include a forloop to rotate() a ellipse() with parameters (0, 0, 20, 80) by PI/6
+
+
 }
 
